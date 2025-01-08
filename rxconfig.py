d046af7cc16d669683c4042bc5d78bbf996a670c
harimkang <harimkang4422@gmail.com>
2025-01-08 18:27:25 +0900

    Fix actions9

--- rxconfig.py
+++ rxconfig.py
@@ -4,4 +4,7 @@ config = rx.Config(
     app_name="portfolio_web",
     base_path="/harimkang-pages",
     frontend_only=True,
+    build_path=".web",
+    static_dir=".web/_static",
+    env=rx.Env.PROD,
 )
