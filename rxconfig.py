8743f2ed59d85a3126487443064f1b88eeb9995d
harimkang <harimkang4422@gmail.com>
2025-01-08 19:02:31 +0900

    Fix actions19

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio_web",
-    base_path="",
+    base_path="/harimkang-pages",
     frontend_only=True,
     build_path=".web",
     static_dir="public",
